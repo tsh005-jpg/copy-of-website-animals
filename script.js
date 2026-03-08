ea7a8943bae7b1d032b4e4f618ee7b0243717cdc
tsh005-jpg <damoyfriend505@gmail.com>
2026-03-08 22:45:17 +0600

    add slides, cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const slides = document.querySelectorAll('.slide');
+const logoSlides = document.querySelectorAll('.slide');
 let current = 0;
 
 setInterval(() => {
@@ -16,13 +16,51 @@ window.addEventListener('scroll', () => {
     logo.style.height = '65px';
     logo.style.width = '65px';
     headerTop.style.opacity = '0';
-    headerTop.style.height = '0';
+    headerTop.style.maxHeight = '0';
     headerTop.style.pointerEvents = 'none';
   } else {
     logo.style.height = '87px';
     logo.style.width = '87px';
     headerTop.style.opacity = '1';
-    headerTop.style.height = '44px';
+    headerTop.style.maxHeight = '44px';
     headerTop.style.pointerEvents = 'auto';
   }
 });
+
+const heroSlidesContainer = document.querySelector('.hero-slides');
+const slides = document.querySelectorAll('.hero-slide');
+const dotsContainer = document.querySelector('.hero-dots');
+let currentSlide = 0;
+let interval;
+
+slides.forEach((_, i) => {
+  const dot = document.createElement('div');
+  dot.classList.add('hero-dot');
+  if (i === 0) dot.classList.add('active');
+  dot.addEventListener('click', () => goToSlide(i));
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll('.hero-dot');
+
+function goToSlide(index) {
+  dots[currentSlide].classList.remove('active');
+
+  currentSlide = index;
+  heroSlidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+  dots[currentSlide].classList.add('active');
+
+  resetInterval();
+}
+
+function nextSlide() {
+  const next = (currentSlide + 1) % slides.length;
+  goToSlide(next);
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(nextSlide, 5000);
+}
+
+resetInterval();
